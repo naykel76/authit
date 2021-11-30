8f8cc33b8cf2b08fe900d2c33cab43a19bc30e6e
naykel76 <32632005+naykel76@users.noreply.github.com>
2021-12-01 09:27:22 +1000

    add container

--- resources/views/user/dashboard-layout.blade.php
+++ resources/views/user/dashboard-layout.blade.php
@@ -1,32 +1,36 @@
 <x-gotime-app-layout layout="{{ config('naykel.template') }}">
 
-    <div class="row">
+    <div class="container">
 
-        <div class="col-md-25">
+        <div class="row">
 
-            <div class="bx">
-                <div class="tac">
-                    <img class="wh200 round" src="{{ auth()->user()->avatarUrl() }}" alt="Profile Photo">
+            <div class="col-md-25">
+
+                <div class="bx">
+                    <div class="tac">
+                        <img class="wh200 round" src="{{ auth()->user()->avatarUrl() }}" alt="Profile Photo">
+                    </div>
                 </div>
+
+                <div class="bx mt-2">
+
+                    <x-authit::user-navigation />
+
+                </div>
+
             </div>
 
-            <div class="bx mt-2">
+            <div class="col-md-75 pl-2">
 
-                <x-authit::user-navigation />
+                @isset($title)
+
+                    <h1 {{ $title->attributes->class([]) }}> {{ $title }} </h1>
+
+                @endisset
+
+                {{ $slot ?? null }}
 
             </div>
-
-        </div>
-
-        <div class="col-md-75 pl-2">
-
-            @isset($title)
-
-                <h1 {{ $title->attributes->class([]) }}> {{ $title }} </h1>
-
-            @endisset
-
-            {{ $slot ?? null }}
 
         </div>
 
