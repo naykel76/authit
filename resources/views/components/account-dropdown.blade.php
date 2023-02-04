c50942abe2ab29051bb7be582030c1edc9776c1b
Nathan <nathan@naykel.com.au>
2023-02-05 04:35:07 +1000

    tweak account-dropdown for better style

--- resources/views/components/account-dropdown.blade.php
+++ resources/views/components/account-dropdown.blade.php
@@ -2,7 +2,8 @@
 
     @auth
 
-        <div class="flex va-c">
+        <div class="flex va-c py-025">
+
             <img class="wh-2 round" src="{{ auth()->user()->avatarUrl() }}" alt="Profile Photo">
 
             <div class="ml-075">
@@ -13,7 +14,7 @@
 
         </div>
 
-        <div class="dd-content bx pxy-0">
+        <div class="dd-content bx pxy-0 mt-025">
             <x-authit::user-navigation />
         </div>
 
